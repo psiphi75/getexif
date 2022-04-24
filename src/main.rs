b1d8ab2a8823882b5c3c5d4337ead26cf1e06be2
Simon Werner <simonwerner@gmail.com>
2022-04-24 14:31:27 +1200

    - Add Exif reading - draft

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,27 @@ fn jpeg_to_metadata(path: &std::path::Path) -> std::io::Result<String> {
     println!("created_time={:?}", to_utc(data.created()?));
     println!("modified_time={:?}", to_utc(data.modified()?));
 
+    let file = std::fs::File::open(path)?;
+    let mut bufreader = std::io::BufReader::new(&file);
+    let exifreader = exif::Reader::new();
+    let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+    let f = exif
+        .get_field(exif::Tag::Orientation, exif::In::PRIMARY)
+        .unwrap();
+    println!("orientation={:?}", f.value);
+
+    let f = exif
+        .get_field(exif::Tag::DateTimeOriginal, exif::In::PRIMARY)
+        .unwrap();
+    println!("capture_time={:?}", f.value);
+
+    let f = exif.get_field(exif::Tag::Model, exif::In::PRIMARY).unwrap();
+    println!("camera_model={:?}", f.value);
+
+    let f = exif
+        .get_field(exif::Tag::BodySerialNumber, exif::In::PRIMARY)
+        .unwrap();
+    println!("camera_serial={:?}", f.value);
     Ok("".to_string())
 }
 
